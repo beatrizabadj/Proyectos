54dd5aa62cf8cefcba87a639b75c392970f39dee
beatrizabadj <beatrizabadjim@gmail.com>
2025-09-15 18:13:51 +0200

    update toReadPage

--- React/library-project-react/client/src/pages/ToReadPage.jsx
+++ React/library-project-react/client/src/pages/ToReadPage.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import SidebarMenu from '../components/SidebarMenu';
 import ToggleSwitch from '../components/ToggleSwitch';
+import { useBooks } from '../context/books.context';
+import BooksList from '../components/BooksList';
+
 function ToReadPage() {
+
+  const { pendingBooks, fetchBooks, handleDeleteBook, handleDeleteAllBooks, handleAddBook} = useBooks();
+  
+  useEffect(()=> {
+    fetchBooks();
+  },[]);
+
   return (
     <main>
       <SidebarMenu/>
       <ToggleSwitch/>
+      <section>
+
       <h1>Books yet to read!</h1>
+      <BooksList type="to-read"/>
+      {pendingBooks.length > 0 && (
+        <button onClick={handleDeleteAllBooks}>Borrar todos</button>
+      )}
+      </section>
     </main>
   )
 }
